09823327437c8b49e32ce62613df092ace71b45b
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2023-04-14 16:59:16 +0100

    Show how ContextNameResolver can reference Req.AcceptLanguage Bump version to depend on 1.7.1

--- SempareTemplateEngineHorseDemo.dpr
+++ SempareTemplateEngineHorseDemo.dpr
@@ -29,17 +29,48 @@ begin
   Current := ACurrent;
 end;
 
+var
+  LPort: integer;
+  LDemos: TArray<TDemo>;
+
 begin
+  LDemos := [ //
+    TDemo.Create('Web Broker', 'https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Creating_WebBroker_Applications', 'https://github.com/sempare/sempare-delphi-template-engine/tree/main/demo/WebBrokerStandalone'), //
+    TDemo.Create('Horse', 'https://github.com/HashLoad/horse', 'https://github.com/sempare/sempare-delphi-template-engine-horse-demo', true) //
+    ];
+
+  Template.Resolver.ContextNameResolver := function(const AName: string; const AContext: TTemplateValue): string
+    var
+      LLang: string;
+      LReq: THorseRequest;
+    begin
+      LReq := AContext.AsType<THorseRequest>;
+      if LReq.Headers.TryGetValue('Accept-Language', LLang) then
+      begin
+        LLang := LLang.Substring(0, 2);
+      end;
+      if LLang.IsEmpty then
+        exit(AName)
+      else
+        exit(AName + '_' + LLang);
+    end;
+
+  LPort := 8080;
+  THorseProvider.KeepConnectionAlive := true;
+
+  writeln('This is the Sempare Template Engine Horse Demo');
+  writeln('');
+  writeln(Format('This is using the Sempare Template Engine v%s', [Template.Version]));
+  writeln('');
+  writeln('The server is running on port ' + inttostr(LPort));
+  writeln('');
+  writeln(Format('Connect your browser to http://localhost:%d', [LPort]));
+  writeln('');
+  writeln('Press Ctrl-C to terminate.');
   THorse.Get('/',
     procedure(Req: THorseRequest; Res: THorseResponse)
-    var
-      LDemos: TArray<TDemo>;
     begin
-      LDemos := [ //
-        TDemo.Create('Web Broker', 'https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Creating_WebBroker_Applications', 'https://github.com/sempare/sempare-delphi-template-engine/tree/main/demo/WebBrokerStandalone'), //
-        TDemo.Create('Horse', 'https://github.com/HashLoad/horse', 'https://github.com/sempare/sempare-delphi-template-engine-horse-demo', true) //
-        ];
-      Res.Send(TTemplateRegistry.Instance.Eval('index', LDemos));
+      Res.Send(Template.ResolveWithContext('index', LDemos, Req));
     end);
 
   THorse.Get('/form',
@@ -56,8 +87,7 @@ begin
         TField.Create('Email', 'email', 'TEmail') //
         ];
       LTemplateData.Buttons := [TButton.Create('Submit', 'submit')];
-      Res.Send(TTemplateRegistry.Instance.Eval('dynform', LTemplateData));
-
+      Res.Send(Template.ResolveWithContext('dynform', LTemplateData, Req));
     end);
 
   THorse.Post('/form',
@@ -68,15 +98,15 @@ begin
       LFormData.firstname := Req.ContentFields['firstname'];
       LFormData.lastname := Req.ContentFields['lastname'];
       LFormData.email := Req.ContentFields['email'];
-      Res.Send(TTemplateRegistry.Instance.Eval('submitted', LFormData));
+      Res.Send(Template.ResolveWithContext('submitted', LFormData, Req));
     end);
 
   THorse.All('/*',
     procedure(Req: THorseRequest; Res: THorseResponse)
     begin
-      Res.Send(TTemplateRegistry.Instance.Eval('error404'));
+      Res.Send(Template.ResolveWithContext('error404', Req));
     end);
 
-  THorse.Listen(8080);
+  THorse.Listen(LPort);
 
 end.
